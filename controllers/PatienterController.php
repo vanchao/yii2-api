d88ca56b49c23007e39a0bad4ef1924795560012
fanchao <van.chao@hotmail.com>
2017-07-26 15:38:53 +0800

    111

---
+++ controllers/PatienterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+class PatienterController extends \yii\web\Controller
+{
+    public function actionIndex($param)
+    {
+        $data = [
+            ['id' => 1, 'realname' => '李珍', 'idencardnum' => '431339198010097761', 'treatmentnum' => '78901103'],
+            ['id' => 2, 'realname' => '刘泽', 'idencardnum' => '177960199003163917', 'treatmentnum' => '91783301'],
+            ['id' => 1, 'realname' => '郭帅', 'idencardnum' => '276513199606209103', 'treatmentnum' => '81901133'],
+        ];
+        echo json_encode($data);
+        // Yii::$app->response->format = Response::FORMAT_JSON;
+    }
+
+}
