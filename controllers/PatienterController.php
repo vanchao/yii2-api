420481f408663bc509e4cada066f195d677633c2
fanchao <van.chao@hotmail.com>
2017-07-26 15:58:00 +0800

    Chane sample data

--- controllers/PatienterController.php
+++ controllers/PatienterController.php
@@ -9,7 +9,7 @@ class PatienterController extends \yii\web\Controller
         $data = [
             ['id' => 1, 'realname' => '李珍', 'idencardnum' => '431339198010097761', 'treatmentnum' => '78901103'],
             ['id' => 2, 'realname' => '刘泽', 'idencardnum' => '177960199003163917', 'treatmentnum' => '91783301'],
-            ['id' => 1, 'realname' => '郭帅', 'idencardnum' => '276513199606209103', 'treatmentnum' => '81901133'],
+            ['id' => 3, 'realname' => '郭帅', 'idencardnum' => '276513199606209103', 'treatmentnum' => '81901133'],
         ];
         echo json_encode($data);
         // Yii::$app->response->format = Response::FORMAT_JSON;
